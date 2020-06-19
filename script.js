435f9eedb45963ad2e9b8c214d34d1c6de974340
Sheila Kerber <sheilask.bio@gmail.com>
2020-06-19 09:59:52 -0300

    altera script para array e modifica css

--- script.js
+++ script.js
@@ -7,17 +7,22 @@ function autenticar() {
         .value
     let res = document.getElementById("resultado")
 
+    let logins = ["joao", "caio", "bruno"]
+    let senhas = ["1234", "senha", "senha123"]
+
     //msg senha e login corretos
-    if (login === "joao" && senha === "1234" || login === "caio" && senha === "senha" || login === "bruno" && senha === "senha123") {
-        res.innerHTML = "Logado!"
-        console.log("logado")
-        document.getElementById("login").value = ""
-        document.getElementById("senha").value = ""
-        return
+    for (i = 0; i < logins.length; i++) {
+        if (login == logins[i] && senha == senhas[i]) {
+            res.innerHTML = "Logado!"
+            console.log("logado")
+            document.getElementById("login").value = ""
+            document.getElementById("senha").value = ""
+            return
+        }
     }
 
     //msg login inválido
-    if (login !== "joao" && senha === "1234" || login !== "caio" && senha === "senha" || login !== "bruno" && senha === "senha123") {
+    if (!logins.includes(login)) {
         res.innerHTML = "[Erro] Login inválido!"
         console.log("login inválido")
         document.getElementById("login").value = ""
@@ -26,7 +31,7 @@ function autenticar() {
     }
 
     //msg senha inválida
-    if (login === "joao" && senha !== "1234" || login === "caio" && senha !== "senha" || login === "bruno" && senha !== "senha123") {
+    if (!senhas.includes(senha)) {
         res.innerHTML = "[Erro] Senha inválida!"
         console.log("senha inválida")
         document.getElementById("login").value = ""
@@ -34,6 +39,7 @@ function autenticar() {
         return
     }
 
+    //msg senha e login inválidos
     res.innerHTML = "[Erro] Login e senha inválidos!"
     document.getElementById("login").value = ""
     document.getElementById("senha").value = ""
